cf4e55da85935527f4ba337fc306a8748fe34898
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2026-03-03 17:07:39 +0700

    Update b.sh

--- b.sh
+++ b.sh
@@ -2,6 +2,6 @@
 
 apt update && apt upgrade -y
 apt install openjdk-21-jdk libgtk-3-0 libx11-6 libxtst6 libxrender1 libfontconfig1 -y
-pt install dbus-x11 -y 
+apt install dbus-x11 -y 
 
 apt instal openbox -y
