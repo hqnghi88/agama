79ea7099dee791dc7130c5ac7e848237496819e0
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2026-03-03 17:59:44 +0700

    Update c.sh

--- c.sh
+++ c.sh
@@ -2,19 +2,14 @@
 cd /data/data/com.termux/files/home/gama
 service dbus start
 export DISPLAY=: 0
- 
- 
-
 export SHARED_MEMORY_DIR=/dev/shm
 mkdir -p $SHARED_MEMORY_DIR
 chmod 777 $SHARED_MEMORY_DIR
 
-export LIBGL_ALWAYS_SOFTWARE=1
-export GDK_ BACKEND=x11
-export SHARED_MEMORY_DIR=/dev/shm
+export GDK_BACKEND=x11
 termux-x11 :0 & 
 
 # Run openbox if not already running
 pgrep openbox | openbox &
 
-GDK_BACKEND=x11 ./Gama
+./Gama
