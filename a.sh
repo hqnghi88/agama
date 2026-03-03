8741c0bd7026cee58c57b9e08458c4560a39e122
HQNghi <hqnghi88@gmail.com>
2026-03-03 15:30:42 +0700

    chore: Update `gama` application download URL in `a.sh` to the latest draft release.

--- a.sh
+++ a.sh
@@ -1,7 +1,7 @@
 
 pkg update && pkg install proot-distro
 
-curl -L https://github.com/hqnghi88/agama/releases/download/untagged-cb5ae4f6e67482042127/gama.application-linux.gtk.aarch64.tar.gz | tar -xz
+curl -L https://github.com/hqnghi88/agama/releases/download/draft-20260303080535/gama.application-linux.gtk.aarch64.tar.gz | tar -xz
 
 proot-distro install ubuntu
 proot-distro login ubuntu
