6e30a2eb2536f044835fd31a792835dd1bd49530
HQNghi <hqnghi88@gmail.com>
2026-03-03 16:47:20 +0700

    chore: Add `-y` flag to `apt upgrade` command in `b.sh` for non-interactive execution.

--- b.sh
+++ b.sh
@@ -1,6 +1,6 @@
 #pkg install termux-x11-shell
 
-apt update && apt upgrade
+apt update && apt upgrade -y
 apt install openjdk-21-jdk libgtk-3-0 libx11-6 libxtst6 libxrender1 libfontconfig1 -y
 pt install dbus-x11 -y 
 
