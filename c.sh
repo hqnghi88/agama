796096f19df886ae22b4127dbb53628392e8fd8a
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2026-03-03 18:12:50 +0700

    Update c.sh

--- c.sh
+++ c.sh
@@ -1,16 +1,14 @@
-#pkg install termux-x11-shell
 cd /data/data/com.termux/files/home/gama
 service dbus start
-export DISPLAY=: 0
 export SHARED_MEMORY_DIR=/dev/shm
 mkdir -p $SHARED_MEMORY_DIR
 chmod 777 $SHARED_MEMORY_DIR
 
 export GDK_BACKEND=x11
 termux-x11 :0 & 
-sleep 3
+sleep 6
 
 # Run openbox if not already running
 pgrep openbox | openbox &
-
+export DISPLAY=: 0
 ./Gama
