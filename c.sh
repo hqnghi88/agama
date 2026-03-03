b861ff4af9a5b6e1fcf7a97cab12a647d933205f
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2026-03-03 18:21:33 +0700

    Update c.sh

--- c.sh
+++ c.sh
@@ -3,12 +3,12 @@ service dbus start
 export SHARED_MEMORY_DIR=/dev/shm
 mkdir -p $SHARED_MEMORY_DIR
 chmod 777 $SHARED_MEMORY_DIR
-
+export DISPLAY=:0
 export GDK_BACKEND=x11
 termux-x11 :0 & 
 sleep 6
 
 # Run openbox if not already running
 pgrep openbox | openbox &
-export DISPLAY=: 0
+
 ./Gama
