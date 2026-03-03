dd2950452e1b930d3905ae8e5a3f1ba0084210ce
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2026-03-03 17:16:19 +0700

    Fix typo in package installation command

--- b.sh
+++ b.sh
@@ -4,4 +4,4 @@ apt update && apt upgrade -y
 apt install openjdk-21-jdk libgtk-3-0 libx11-6 libxtst6 libxrender1 libfontconfig1 -y
 apt install dbus-x11 -y 
 
-apt instal openbox -y
+apt install openbox -y
