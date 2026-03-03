00a855ef03689b781a117e1f3c5f6b5078309e5e
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2026-03-03 17:40:08 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -4,6 +4,8 @@ pkg update
 pkg install proot-distro -y
 pkg install x11-repo -y
 pkg install termux-x11-nightly -y
+mkdir gama
+cd gama
 curl -L https://github.com/hqnghi88/agama/releases/download/draft-20260303080535/gama.application-linux.gtk.aarch64.tar.gz | tar -xz
 
 proot-distro install ubuntu
