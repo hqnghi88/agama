77a8f8423ef97fe36993b444f10615576daaef97
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2026-03-07 17:54:59 +0700

    Remove login command for proot-distro Ubuntu
    Remove proot-distro login command for Ubuntu.

--- a.sh
+++ a.sh
@@ -9,4 +9,3 @@ cd gama
 curl -L https://github.com/hqnghi88/agama/releases/download/draft-20260303080535/gama.application-linux.gtk.aarch64.tar.gz | tar -xz
 
 proot-distro install ubuntu
-proot-distro login ubuntu
