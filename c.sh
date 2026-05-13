4bae9d95149acdd6297b95a6c45106f133d455bb
HQNghi <hqnghi88@gmail.com>
2026-05-13 23:20:59 +0700

    fix: remove unnecessary environment variable exports for GAMA stability

--- c.sh
+++ c.sh
@@ -10,18 +10,6 @@ sleep 3
 
 # Run openbox if not already running
 pgrep openbox | openbox &
- 
-export GALLIUM_DRIVER=llvmpipe
-export MESA_LOADER_DRIVER_OVERRIDE=llvmpipe
-export LIBGL_ALWAYS_SOFTWARE=1
-
-# 2. Version Overrides (Stability)
-export MESA_GL_VERSION_OVERRIDE=3.3
-export MESA_GLSL_VERSION_OVERRIDE=330
-
-# 3. X11 Stability
-export GDK_BACKEND=x11
-export QT_X11_NO_MITSHM=1
 
 # 4. Launch with JVM memory fixes
 ./Gama
