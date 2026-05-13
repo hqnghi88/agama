e43e51a179707f58fd44ebc02c05f6ed1c6f48bd
HQNghi <hqnghi88@gmail.com>
2026-05-13 20:40:50 +0700

    fix: correct OpenJDK version in installation script

--- b.sh
+++ b.sh
@@ -1,7 +1,7 @@
 #pkg install termux-x11-shell
 
 apt update && apt upgrade -y
-apt install openjdk-25-jdk libgtk-3-0 libx11-6 libxtst6 libxrender1 libfontconfig1 -y
+apt install openjdk-21-jdk libgtk-3-0 libx11-6 libxtst6 libxrender1 libfontconfig1 -y
 apt install dbus-x11 -y 
 
 apt install openbox -y
