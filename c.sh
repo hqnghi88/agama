9e61230afbef427b2cdac3ed801bbda2aba9d1b7
HQNghi <hqnghi88@gmail.com>
2026-05-13 23:08:41 +0700

    fix: update GAMA installation script to set Gallium driver and optimize environment variables for stability

--- c.sh
+++ c.sh
@@ -10,5 +10,18 @@ sleep 3
 
 # Run openbox if not already running
 pgrep openbox | openbox &
+ 
+export GALLIUM_DRIVER=llvmpipe
+export MESA_LOADER_DRIVER_OVERRIDE=llvmpipe
+export LIBGL_ALWAYS_SOFTWARE=1
 
-./Gama
+# 2. Version Overrides (Stability)
+export MESA_GL_VERSION_OVERRIDE=3.3
+export MESA_GLSL_VERSION_OVERRIDE=330
+
+# 3. X11 Stability
+export GDK_BACKEND=x11
+export QT_X11_NO_MITSHM=1
+
+# 4. Launch with JVM memory fixes
+./Gama -vmargs -Dsun.java2d.opengl=false -XX:-UseCompressedOops
